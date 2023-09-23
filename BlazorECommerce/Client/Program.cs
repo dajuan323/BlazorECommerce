06905f3983a19b7c91e6411377b308f0acc296aa
dajuan323 <macaro.martin@gmail.com>
2023-09-23 18:17:01 -0400

    Product Details, Categories, & Seeding More Products.

--- BlazorECommerce/Client/Program.cs
+++ BlazorECommerce/Client/Program.cs
@@ -1,4 +1,7 @@
+global using BlazorECommerce.Shared;
+global using System.Net.Http.Json;
 using BlazorECommerce.Client;
+using BlazorECommerce.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorECommerce/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,37 @@
+﻿namespace BlazorECommerce.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly DataContext _dataContext;
+        public ProductService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        {
+            var response = new ServiceResponse<Product>();
+            var product = await _dataContext.Products.FindAsync(productId);
+            if (product == null)
+            {
+                response.Success = false;
+                response.Message = "Sorry, this product does not exist.";
+            }
+            else
+            {
+                response.Data = product;
+            }
+            return response;
+        }
+
+        public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
+        {
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = await _dataContext.Products.ToListAsync(),
+            };
+
+            return response;
+        }
+    }
+}
